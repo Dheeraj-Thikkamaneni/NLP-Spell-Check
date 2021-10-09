85057149c3c54b2db20dd86a052c26d31f74bd61
Dheeraj_T <74044337+Dheeraj-Thikkamaneni@users.noreply.github.com>
2021-10-08 22:19:24 -0500

    Update spellcheck.py
    added edit operations and word probability methods, gives suggestions for each wrongly spelled word

--- spellcheck.py
+++ spellcheck.py
@@ -35,3 +35,51 @@ def replace(word):
 def insert(word):
     letters = string.ascii_lowercase
     return [l + c + r for l,r in split(word) for c in letters]  
+
+
+def level_one_edit(word): # perform all operations
+    return set(delete(word) + swap(word) + replace(word) + insert(word))
+
+def level_two_edit(word):
+    return set(e2 for e1 in level_one_edit(word) for e2 in level_one_edit(e1))
+
+def correct_spelling(word, text, word_probability):
+    guesses =[]
+    if word in text:
+        #print(word)
+        return guesses
+    
+    suggestions = level_one_edit(word) or level_two_edit(word) or [word]
+    best_guesses = [w for w in suggestions if w in text]
+    return [(w, word_probability[w]) for w in best_guesses]
+
+
+class SpellCheck():
+    def __init__(self):
+        
+        my_dict = enchant.Dict("en_US")
+        words = read_file('t5.txt') # not as same as above words list
+        unique_words = set(words)
+        word_count = Counter(words) # words_count is a dictionary, counts number of each word occurence and stores in dictionary, eg: 'the':613
+        total_word_count = float(sum(word_count.values())) # values return value in dictionary
+        word_probability = { word: word_count[word] / total_word_count for word in word_count.keys()} # dict comprehension, word stores each word probability
+
+        
+        iwords = input("Enter a sentence: ").strip().lower().split()
+        print('\n')
+        
+        guesses = []
+        
+        for word in iwords:
+
+            guesses = correct_spelling(word, unique_words, word_probability)
+            #print(guesses)
+            if len(guesses) !=0:
+                print(word)
+                print("suggestions")
+                for w in guesses:
+                    if my_dict.check(w[0]):
+                        
+                        print(w[0]) # printing suggestions
+                    
+obj = SpellCheck()
