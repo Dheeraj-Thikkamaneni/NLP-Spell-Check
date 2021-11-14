51242ff229dc56f330b1a950f8dd915cd13250ce
Dheeraj_T <74044337+Dheeraj-Thikkamaneni@users.noreply.github.com>
2021-11-13 20:57:59 -0600

    Update list_to_file.py

--- list_to_file.py
+++ list_to_file.py
@@ -1,14 +1,14 @@
 wordlist = []
 text = ""
 
-for i in range(1, 2):
-    with open("english_list_"+str(i)+".txt", encoding='utf-8') as file:
+for i in range(1, 627):
+    with open("for_irish\irish_list_"+str(i)+".txt", encoding='utf-8') as file:
         for line in file:
             text += line
+            print(i)
 
 
-with open('english_final.txt', 'w', encoding='utf-8') as file:
+with open('irish_final.txt', 'w', encoding='utf-8') as file:
     file.write(str(text))
-
 
 print("END")
