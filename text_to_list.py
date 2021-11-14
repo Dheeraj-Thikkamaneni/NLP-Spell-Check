6f63ff13175e03823bcb5655789a7592a3c3871c
Dheeraj_T <74044337+Dheeraj-Thikkamaneni@users.noreply.github.com>
2021-11-13 20:56:56 -0600

    Update text_to_list.py

--- text_to_list.py
+++ text_to_list.py
@@ -1,24 +1,24 @@
 import re
 
 count = 0
-with open("english.txt", encoding='utf-8') as file:
+
+with open("irish.txt", encoding='utf-8') as file:
     for line in file:
         count = count + 1
 
 print("Number of lines: {}".format(count))
-
 
 count = 0
 text = ""
 r = 1
 words = []
 
-with open("english.txt", encoding='utf-8') as file:
+with open("irish.txt", encoding='utf-8') as file:
     for line in file:
         count = count + 1
-        words = words + \
-            re.findall("([\w]+[-]?[']?[\w]*)", line, flags=re.IGNORECASE)
-        files = open('english_list_'+str(r)+'.txt', 'w', encoding='utf-8')
+        words = words + re.findall("([\w]+[-]?[']?[\w]*)", line, flags=re.IGNORECASE)
+        
+        files = open('for_irish\irish_list_aa' + str(r) + '.txt', 'w', encoding='utf-8')
         files.write(str(words))
         print(count)
         if count % 5000 == 0:
