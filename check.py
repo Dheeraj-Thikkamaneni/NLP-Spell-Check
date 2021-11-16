069d3694415613169bca85f27a7c809098d6d1d9
gantlavamshikrishnareddy <38282111+gantlavamshikrishnareddy@users.noreply.github.com>
2021-11-16 16:05:01 -0600

    Update check.py

--- check.py
+++ check.py
@@ -1,3 +1,28 @@
+import numpy as np
+import pandas as pd
+import re # Regular Expressions
+from collections import Counter
+import string
+import enchant
+from flask import *
+from flask import render_template
+
+from textblob import TextBlob
+from gingerit.gingerit import GingerIt
+from autocorrect import Speller
+
+
+
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.readlines()
+        words = []
+        for line in text:
+            words = words + re.findall(r'\w+', line.lower()) # RE for making a words list from file
+            
+    return words        
+        
 # edit operations,    insert, delete, swap, replace
 
 def split(word):
